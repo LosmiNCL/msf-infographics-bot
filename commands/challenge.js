25aa4c3ea402468e3eab5518ed74ce50a4412fad
LosmiNCL <120345811+LosmiNCL@users.noreply.github.com>
2022-12-29 01:22:30 +0100

    Update challenge.js

--- commands/challenge.js
+++ commands/challenge.js
@@ -43,7 +43,6 @@ module.exports = {
 			{name: 'Detailed Guide', value: postUrl},
 			{name: 'Youtube video', value: youtubeUrl}
 		)
-        .setImage(imageUrl)
 
         interaction.channel.send({embeds: [embedSent]})
 
