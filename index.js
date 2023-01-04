1e1cfbca3c805e8419a1cc07d666d36719f7c863
LosmiNCL <120345811+LosmiNCL@users.noreply.github.com>
2023-01-04 09:27:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
-var admin = require("firebase-admin");
-require('dotenv').config();
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -27,28 +25,12 @@ const firebaseConfig = {
   };
 
 // Initialize Firebase
-admin.initializeApp({
-	credential: admin.credential.cert({
-	  projectId: process.env.FIREBASE_PROJECT_ID,
-	  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-	  // replace `\` and `n` character pairs w/ single `\n` character
-	  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-	}),
-	// The database URL depends on the location of the database
-	databaseURL: "https://msf-discord-bot-default-rtdb.europe-west1.firebasedatabase.app",
-	databaseAuthVariableOverride: {
-		uid: process.env.FOR_AUTH_UID
-	  }
-  })
-
-console.log(process.env.FIREBASE_PROJECT_ID);
-console.log(process.env.FIREBASE_CLIENT_EMAIL);
-console.log(process.env.FIREBASE_PRIVATE_KEY);
-
-var db = admin.database();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getDatabase();
 const dbref = ref(db);
 
-module.exports = {firebase, database, admin, db, dbref}
+module.exports = {firebase, database, app, auth, db, dbref}
 
 client.commands = new Collection();
 
