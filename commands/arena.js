c7b7384ac132683d0d642acb92f8ad06e105660f
LosmiNCL <120345811+LosmiNCL@users.noreply.github.com>
2023-01-04 01:13:30 +0100

    More choices

--- commands/arena.js
+++ commands/arena.js
@@ -13,10 +13,15 @@ module.exports = {
 			.addChoices(
 				{name: 'arena meta horsemen', value: 'arena meta horsemen'},
 				{name: 'arena meta eternals', value: 'arena meta eternals'},
+				{name: 'arena defense horsmans dormammu', value: 'arena defense horsmans dormammu'},
+				{name: 'arena defense tangled web emma dormammu red hulk', value: 'arena defense tangled web emma dormammu red hulk'},
+				{name: 'arena defense emma dormammu rogue', value: 'arena defense emma dormammu rogue'},
+				{name: 'arena defense tangled web emma eternals', value: 'arena defense tangled web emma eternals'}
 			)),
 	async execute(interaction) {
 		var postUrl = null;
 		var youtubeUrl = null;
+		var imageUrl = null;
 
 		await get(child(dbref, '/infographics/arena/' + interaction.options.getString("arena-team") + '/post-url')).then((snapshot) => {
 			if (snapshot.exists()){
@@ -27,23 +32,43 @@ module.exports = {
 			}
 		})
 
-		await get(child(dbref, '/infographics/arena/' + interaction.options.getString("arena-team") + '/youtube-url')).then((snapshot) => {
-			if (snapshot.exists()){
-				youtubeUrl = snapshot.val();
+		if(interaction.options.getString("arena-team") === 'arena meta horsemen' || interaction.options.getString("arena-team") === 'arena meta eternals'){
+			await get(child(dbref, '/infographics/arena/' + interaction.options.getString("arena-team") + '/youtube-url')).then((snapshot) => {
+				if (snapshot.exists()){
+					youtubeUrl = snapshot.val();
+	
+				} else {
+					youtubeUrl = "Not found";
+				}
+			})
 
-			} else {
-				youtubeUrl = "Not found";
-			}
-		})
+			const embedSent = new EmbedBuilder()
+			.setTitle(interaction.options.getString("arena-team"))
+			.setFields(
+				{name: 'Detailed Guide', value: postUrl},
+				{name: 'Youtube video', value: youtubeUrl}
+			)
+	
+			interaction.channel.send({embeds: [embedSent]})
+		} else {
+			await get(child(dbref, '/infographics/arena/' + interaction.options.getString("arena-team") + '/image-url')).then((snapshot) => {
+				if (snapshot.exists()){
+					imageUrl = snapshot.val();
+	
+				} else {
+					imageUrl = "Not found";
+				}
+			})
 
-		const embedSent = new EmbedBuilder()
-		.setTitle(interaction.options.getString("arena-team"))
-		.setFields(
-			{name: 'Detailed Guide', value: postUrl},
-			{name: 'Youtube video', value: youtubeUrl}
-		)
-
-        interaction.channel.send({embeds: [embedSent]})
+			const embedSent = new EmbedBuilder()
+			.setTitle(interaction.options.getString("arena-team"))
+			.setFields(
+				{name: 'Detailed Guide', value: postUrl},
+			)
+			.setImage(imageUrl)
+	
+			interaction.channel.send({embeds: [embedSent]})
+		}
 
 		await interaction.reply('Good luck Commander!');
 	},
