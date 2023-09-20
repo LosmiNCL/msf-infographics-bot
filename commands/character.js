109803e162b2327b5eb5f6b64268ac59630d55f8
LosmiNCL <svemir2212@gmail.com>
2023-09-20 16:52:04 +0200

    git commit -m "new characters"

--- commands/character.js
+++ commands/character.js
@@ -16,7 +16,7 @@ module.exports = {
         .setAutocomplete(true)),
     async autocomplete(interaction) {
         const focusedValue = interaction.options.getFocused();
-        const choices = ['iron man infinity war', 'darkhawk', 'ironheart mk ii',
+        const choices = ['juggernaut zombie', 'iron man infinity war', 'darkhawk', 'ironheart mk ii',
                         'tigra' ,'ronin', 'mockingbird', 'photon','ms marvel hard light',
                         'ghost rider robbie','black cat','spider-man big time','vahl' ,'beta ray bill',
                         'sylvie' ,'loki teen' ,'super skrull' ,'korg', 'gwenpool',
