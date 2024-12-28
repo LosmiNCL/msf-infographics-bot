aa4fc36d2342bc5e99bd6e708d3b125d2ab20c38
LosmiNCL <svemir2212@gmail.com>
2024-12-28 22:42:03 +0100

    git commit -m "Added Hercules and Jean Grey"

--- commands/character.js
+++ commands/character.js
@@ -16,7 +16,8 @@ module.exports = {
         .setAutocomplete(true)),
     async autocomplete(interaction) {
         const focusedValue = interaction.options.getFocused();
-        const choices = ['medusa', 'ms marvel classic', 'kahhori', 'ancient one', 'shadow king',
+        const choices = ['hercules', 'jean grey',
+                        'medusa', 'ms marvel classic', 'kahhori', 'ancient one', 'shadow king',
                         'emma frost x-men', 'scarlet witch zombie', 'blade', 'man-thing', 'oath',
                         'odin', 'omega sentinel', 'nimrod', 'sentinel','thanos endgame',
                         'gorr', 'gladiator', 'ares', 'captain britain', 'hank pym',
