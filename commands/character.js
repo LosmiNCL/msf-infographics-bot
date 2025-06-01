7c9c2559985ab74cedecb4574f70b97f0ae0e9af
LosmiNCL <svemir2212@gmail.com>
2025-06-01 18:01:33 +0200

    git commit -m "Hellverine, Satana, The Hood"

--- commands/character.js
+++ commands/character.js
@@ -16,7 +16,8 @@ module.exports = {
         .setAutocomplete(true)),
     async autocomplete(interaction) {
         const focusedValue = interaction.options.getFocused();
-        const choices = ['victoria hand', 'songbird', 'hyperion',
+        const choices = ['the hood',
+                        'satana', 'hellverine', 'victoria hand', 'songbird', 'hyperion',
                         'quasar', 'professor xavier', 'hit monkey', 'daredevil modern', 'silver sable',
                         'knull', 'havok', 'lilandra', 'howard the duck', 'peggy carter',
                         'patriot', 'falcon joaquin', 'omega red phoenix force', 'hercules', 'jean grey',
