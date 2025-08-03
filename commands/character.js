43653c603d5fddeda57d762d8c59d920ee0c8f5f
LosmiNCL <svemir2212@gmail.com>
2025-08-03 15:22:11 +0200

    git commit -m "Sebastian Shaw, Azazel, and Rachel Summers added"

--- commands/character.js
+++ commands/character.js
@@ -16,7 +16,8 @@ module.exports = {
         .setAutocomplete(true)),
     async autocomplete(interaction) {
         const focusedValue = interaction.options.getFocused();
-        const choices = ['blastaar', 'invisible woman mcu', 'mister fantastic mcu', 'franklin richards', 'the hood',
+        const choices = ['sebastian shaw', 'azazel', 'rachel summers',
+                        'blastaar', 'invisible woman mcu', 'mister fantastic mcu', 'franklin richards', 'the hood',
                         'satana', 'hellverine', 'victoria hand', 'songbird', 'hyperion',
                         'quasar', 'professor xavier', 'hit monkey', 'daredevil modern', 'silver sable',
                         'knull', 'havok', 'lilandra', 'howard the duck', 'peggy carter',
