9bd77cbf7687da6b9fb21faedd2f1d78ca0f03db
LosmiNCL <svemir2212@gmail.com>
2026-05-06 21:22:18 +0200

    git commit -m "Update 6th May 2026"

--- commands/character.js
+++ commands/character.js
@@ -16,7 +16,7 @@ module.exports = {
         .setAutocomplete(true)),
     async autocomplete(interaction) {
         const focusedValue = interaction.options.getFocused();
-        const choices = ['storm mighty',
+        const choices = ['silver surfer breaker', 'storm mighty',
                         'morph', 'angel', 'annihilus', 'maestro', 'malekith',
                         'executioner', 'high evolutionary', 'eclipse', 'night thrasher', 'speedball',
                         'moon girl', 'nova sam alexander', 'jeff the land shark', 'darkstar', 'ursa major',
--- commands/teams.js
+++ commands/teams.js
@@ -16,7 +16,7 @@ module.exports = {
         .setAutocomplete(true)),
     async autocomplete(interaction) {
         const focusedValue = interaction.options.getFocused();
-        const choices = ['exalted x-men', 'shadow conclave', 'daring warrior', 'champion',
+        const choices = ['omen', 'exalted x-men', 'shadow conclave', 'daring warrior', 'champion',
                         'galactic council', 'retcon', 'winter guard', 'new mutant', 'immortal weapon',
                         'brimstone', 'insidious six', 'secret warrior', 'hellfire club', 'fantastic four mcu',
                         'accursed', 'thunderbolt', 'vigilante', 'starjammer', 'phoenix force',
