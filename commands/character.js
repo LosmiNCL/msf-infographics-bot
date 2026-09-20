27e3e6413ccd5733cc41bea5342e4478f2dc0fdb
LosmiNCL <svemir2212@gmail.com>
2026-09-20 18:27:34 +0200

    git commit -m "20st September 2026 West Coast Avenger"

--- commands/character.js
+++ commands/character.js
@@ -16,7 +16,8 @@ module.exports = {
         .setAutocomplete(true)),
     async autocomplete(interaction) {
         const focusedValue = interaction.options.getFocused();
-        const choices = ['whiplash', 'iron monger', 'ulysses klaue', 'gilgamesh', 'kingo',
+        const choices = ['wasp janet',
+                        'whiplash', 'iron monger', 'ulysses klaue', 'gilgamesh', 'kingo',
                         'thena', 'scorpion breaker', 'blade mighty', 'spider-woman julia', 'rachel cole-alves',
                         'quicksilver symbiote', 'toxin', 'riot', 'silver surfer breaker', 'storm mighty',
                         'morph', 'angel', 'annihilus', 'maestro', 'malekith',
--- commands/teams.js
+++ commands/teams.js
@@ -16,7 +16,8 @@ module.exports = {
         .setAutocomplete(true)),
     async autocomplete(interaction) {
         const focusedValue = interaction.options.getFocused();
-        const choices = ['iron raider', 'timeless eternal', 'dread', 'amazing avenger', 'symbiote six',
+        const choices = ['west coast avenger',
+                        'iron raider', 'timeless eternal', 'dread', 'amazing avenger', 'symbiote six',
                         'omen', 'exalted x-men', 'shadow conclave', 'daring warrior', 'champion',
                         'galactic council', 'retcon', 'winter guard', 'new mutant', 'immortal weapon',
                         'brimstone', 'insidious six', 'secret warrior', 'hellfire club', 'fantastic four mcu',
